7e9b4b0530af0fb6b984b3170db090839c74bf33
Jasmin Feldmann <jfeldm@d-velop.de>
2025-07-29 12:15:13 +0200

    minimal working example

---
+++ README.md
@@ -0,0 +1,7 @@
+# d.velop Bits and Burgers Projekt
+Siehe [Bits and Burgers](https://karriere.d-velop.de/bits-burgers/) für mehr Informationen zu dem Format.
+
+Dieses Projekt ist eine mögliche Aufgabe während der Kleingruppenarbeit.
+Es orientiert sich an folgendem Projekt: [Meteors von ThreeDotsLabs](https://github.com/ThreeDotsLabs/meteors/tree/master)
+
+Die genutzten Ressourcen sind ebenfalls dort und unter [Kenney](https://kenney.nl/assets/space-shooter-redux) zu finden.
---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"embed"
+	"image"
+	_ "image/png"
+	"io/fs"
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	Spielbreite = 800
+	Spielhoehe  = 600
+)
+
+//go:embed assets/*
+var assets embed.FS
+
+var PlayerBild = mustLoadImage("assets/player.png")
+var MeteoritenBilder = mustLoadImages("assets/meteors/*.png")
+
+type Vector struct {
+	X float64
+	Y float64
+}
+
+type Meteorit struct {
+	bild     *ebiten.Image
+	position Vector
+	bewegung Vector
+}
+
+type Player struct {
+	bild     *ebiten.Image
+	position Vector
+}
+
+type Game struct {
+	player               *Player
+	meteoriten           []*Meteorit
+	meteoritenSpawnTimer *Timer
+}
+
+func NewPlayer() *Player {
+
+	grenzen := PlayerBild.Bounds()
+	halbeBreite := float64(grenzen.Dx()) / 2
+	halbeHoehe := float64(grenzen.Dy()) / 2
+
+	pos := Vector{
+		X: Spielbreite/2 - halbeBreite,
+		Y: Spielhoehe/2 - halbeHoehe,
+	}
+
+	return &Player{
+		position: pos,
+		bild:     PlayerBild,
+	}
+}
+
+func NewMeteorit() *Meteorit {
+	return &Meteorit{
+		bild: MeteoritenBilder[rand.Intn(len(MeteoritenBilder))],
+		position: Vector{
+			X: float64(rand.Intn(Spielbreite)),
+			Y: 0,
+		},
+		bewegung: Vector{
+			X: 0,
+			Y: float64(rand.Intn(3) + 1), // Random speed between 1 and 3
+		},
+	}
+}
+
+func (m *Meteorit) Update() {
+	// Update meteorite position based on its bewegung vector
+	m.position.X += m.bewegung.X
+	m.position.Y += m.bewegung.Y
+
+	// Check if the meteorite is off-screen and remove it if necessary
+	if m.position.Y > Spielhoehe {
+		// Remove logic can be added here if needed
+	}
+}
+
+func (p *Player) Update() error {
+	geschwindigkeit := 5.0
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		p.position.Y += geschwindigkeit
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		p.position.Y -= geschwindigkeit
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		p.position.X -= geschwindigkeit
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		p.position.X += geschwindigkeit
+	}
+	return nil
+}
+
+func (g *Game) Update() error {
+	g.player.Update()
+
+	g.meteoritenSpawnTimer.Update()
+	if g.meteoritenSpawnTimer.IsReady() {
+		g.meteoritenSpawnTimer.Reset()
+		g.meteoriten = append(g.meteoriten, NewMeteorit())
+	}
+
+	for _, m := range g.meteoriten {
+		m.Update()
+		if m.KollisionsRechteck().istKollidiert(g.player.KollisionsRechteck()) {
+			// Handle collision with player
+			g.GameOver()
+		}
+	}
+
+	return nil
+}
+
+func (g *Game) GameOver() {
+	g.player = NewPlayer()
+	g.meteoriten = nil
+}
+
+func (m *Meteorit) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(m.position.X, m.position.Y)
+	screen.DrawImage(m.bild, op)
+}
+
+func (p *Player) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(p.position.X, p.position.Y)
+	screen.DrawImage(p.bild, op)
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.player.Draw(screen)
+
+	for _, m := range g.meteoriten {
+		m.Draw(screen)
+	}
+}
+
+func (p *Player) KollisionsRechteck() Rect {
+	bounds := p.bild.Bounds()
+	return NewRect(
+		p.position.X,
+		p.position.Y,
+		float64(bounds.Dx()),
+		float64(bounds.Dy()),
+	)
+}
+
+func (m *Meteorit) KollisionsRechteck() Rect {
+	bounds := m.bild.Bounds()
+	return NewRect(
+		m.position.X,
+		m.position.Y,
+		float64(bounds.Dx()),
+		float64(bounds.Dy()),
+	)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return Spielbreite, Spielhoehe
+}
+
+func main() {
+	g := &Game{
+		meteoritenSpawnTimer: NewTimer(2 * time.Second), // Spawn meteoriten every 2 seconds
+		player:               NewPlayer(),
+	}
+
+	err := ebiten.RunGame(g)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mustLoadImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
+
+func mustLoadImages(path string) []*ebiten.Image {
+	matches, err := fs.Glob(assets, path)
+	if err != nil {
+		panic(err)
+	}
+
+	images := make([]*ebiten.Image, len(matches))
+	for i, match := range matches {
+		images[i] = mustLoadImage(match)
+	}
+
+	return images
+}
