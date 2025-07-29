e65a944d1cb37bd2bd79b246cef26fe5d0b4b91a
Jasmin Feldmann <jfeldm@d-velop.de>
2025-07-29 13:22:46 +0200

    remove everything but the bare bones for rendering a player

--- main.go
+++ main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"embed"
+	"github.com/hajimehoshi/ebiten/v2"
 	"image"
 	_ "image/png"
-	"io/fs"
-	"math/rand"
-	"time"
-
-	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
@@ -20,7 +16,6 @@ const (
 var assets embed.FS
 
 var PlayerBild = mustLoadImage("assets/player.png")
-var MeteoritenBilder = mustLoadImages("assets/meteors/*.png")
 
 type Vector struct {
 	X float64
@@ -28,9 +23,6 @@ type Vector struct {
 }
 
 type Meteorit struct {
-	bild     *ebiten.Image
-	position Vector
-	bewegung Vector
 }
 
 type Player struct {
@@ -39,99 +31,18 @@ type Player struct {
 }
 
 type Game struct {
-	player               *Player
-	meteoriten           []*Meteorit
-	meteoritenSpawnTimer *Timer
-}
-
-func NewPlayer() *Player {
-
-	grenzen := PlayerBild.Bounds()
-	halbeBreite := float64(grenzen.Dx()) / 2
-	halbeHoehe := float64(grenzen.Dy()) / 2
-
-	pos := Vector{
-		X: Spielbreite/2 - halbeBreite,
-		Y: Spielhoehe/2 - halbeHoehe,
-	}
-
-	return &Player{
-		position: pos,
-		bild:     PlayerBild,
-	}
-}
-
-func NewMeteorit() *Meteorit {
-	return &Meteorit{
-		bild: MeteoritenBilder[rand.Intn(len(MeteoritenBilder))],
-		position: Vector{
-			X: float64(rand.Intn(Spielbreite)),
-			Y: 0,
-		},
-		bewegung: Vector{
-			X: 0,
-			Y: float64(rand.Intn(3) + 1), // Random speed between 1 and 3
-		},
-	}
-}
-
-func (m *Meteorit) Update() {
-	// Update meteorite position based on its bewegung vector
-	m.position.X += m.bewegung.X
-	m.position.Y += m.bewegung.Y
-
-	// Check if the meteorite is off-screen and remove it if necessary
-	if m.position.Y > Spielhoehe {
-		// Remove logic can be added here if needed
-	}
-}
-
-func (p *Player) Update() error {
-	geschwindigkeit := 5.0
-	if ebiten.IsKeyPressed(ebiten.KeyDown) {
-		p.position.Y += geschwindigkeit
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyUp) {
-		p.position.Y -= geschwindigkeit
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		p.position.X -= geschwindigkeit
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		p.position.X += geschwindigkeit
-	}
-	return nil
+	player *Player
 }
 
 func (g *Game) Update() error {
-	g.player.Update()
-
-	g.meteoritenSpawnTimer.Update()
-	if g.meteoritenSpawnTimer.IsReady() {
-		g.meteoritenSpawnTimer.Reset()
-		g.meteoriten = append(g.meteoriten, NewMeteorit())
-	}
-
-	for _, m := range g.meteoriten {
-		m.Update()
-		if m.KollisionsRechteck().istKollidiert(g.player.KollisionsRechteck()) {
-			// Handle collision with player
-			g.GameOver()
-		}
-	}
-
 	return nil
 }
 
-func (g *Game) GameOver() {
-	g.player = NewPlayer()
-	g.meteoriten = nil
-}
-
-func (m *Meteorit) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(m.position.X, m.position.Y)
-	screen.DrawImage(m.bild, op)
+func NewPlayer() *Player {
+	return &Player{
+		position: Vector{X: 100, Y: 100},
+		bild:     PlayerBild,
+	}
 }
 
 func (p *Player) Draw(screen *ebiten.Image) {
@@ -142,30 +53,6 @@ func (p *Player) Draw(screen *ebiten.Image) {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.player.Draw(screen)
-
-	for _, m := range g.meteoriten {
-		m.Draw(screen)
-	}
-}
-
-func (p *Player) KollisionsRechteck() Rect {
-	bounds := p.bild.Bounds()
-	return NewRect(
-		p.position.X,
-		p.position.Y,
-		float64(bounds.Dx()),
-		float64(bounds.Dy()),
-	)
-}
-
-func (m *Meteorit) KollisionsRechteck() Rect {
-	bounds := m.bild.Bounds()
-	return NewRect(
-		m.position.X,
-		m.position.Y,
-		float64(bounds.Dx()),
-		float64(bounds.Dy()),
-	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -174,8 +61,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	g := &Game{
-		meteoritenSpawnTimer: NewTimer(2 * time.Second), // Spawn meteoriten every 2 seconds
-		player:               NewPlayer(),
+		player: NewPlayer(),
 	}
 
 	err := ebiten.RunGame(g)
@@ -197,18 +83,4 @@ func mustLoadImage(name string) *ebiten.Image {
 	}
 
 	return ebiten.NewImageFromImage(img)
-}
-
-func mustLoadImages(path string) []*ebiten.Image {
-	matches, err := fs.Glob(assets, path)
-	if err != nil {
-		panic(err)
-	}
-
-	images := make([]*ebiten.Image, len(matches))
-	for i, match := range matches {
-		images[i] = mustLoadImage(match)
-	}
-
-	return images
 }
